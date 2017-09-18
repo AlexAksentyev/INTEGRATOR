d2e9e9a37db70b109a706c85f05b3dae08068b92
Alex Aksentyev <alexaksentyev@gmail.com>
2017-09-18 17:41:08 +0200

    added lattice; doesn't track due to negative in sqrt

--- BNL.py
+++ BNL.py
@@ -14,6 +14,7 @@ from importlib import reload
 
 theme_bw()
 
+# hardware parameters
 Lq = 5
 Ls = .15
 
@@ -24,11 +25,14 @@ h = .05
 R = 9.207
 
 #%%
-
+# particle definition
 state0 = [1e-3, -1e-3, 0, 0, 0, 0, 0, 0, 1, 0]
 p = PCL.Particle(state0)
 
 #%%
+
+# lattice elements
+
 DL_25  = ENT.Drift(.25)
 DL_15 = ENT.Drift(.15)
 DL2_2 = ENT.Drift(2.2)
@@ -49,3 +53,39 @@ B = ENT.Wien.computeBStrength(p, R, h)
 WA = ENT.Wien(1.808, 9.297, h, V, B)
 
 
+#%%
+# lattice definition
+
+SS1H2 = [QDS , DL_25 , DL_15 , DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
+         QFS , QFS , DL_25 , DL_15 , #  RF ,
+                                     DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
+         QDS , QDS , DL_25 , DL_15 , DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
+         QFS]
+
+ARC1 = [QFA , DL_25 , Sf , DL_25 , WA , DL_25 , BPM , DL_25] +\
+ [QDA , QDA , DL_25 , Sd , DL_25 , WA , DL_25 , BPM , DL_25 ,
+         QFA , QFA , DL_25 , Sf , DL_25 , WA , DL_25 , BPM , DL_25]*7 +\
+  [QDA , QDA , DL_25 , Sd , DL_25 , WA , DL_25 , BPM , DL_25 ,
+         QFA]
+
+SS2H1 = [QFS , DL_25 , Sf , DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
+         QDS , QDS , DL_25 , Sd , DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
+         QFS , QFS , DL_25 , Sf , DL_25 , DL2_2 , DL_25 , BPM , DL_25]
+
+SS2H2 = [QDS , DL_25 , DL_15 , DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
+         QFS , QFS , DL_25 , DL_15 , DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
+         QDS , QDS , DL_25 , DL_15 , DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
+         QFS]
+
+ARC2 =  [QFA , DL_25 , Sf , DL_25 , WA , DL_25 , BPM , DL_25] +\
+ [QDA , QDA , DL_25 , Sd , DL_25 , WA , DL_25 , BPM , DL_25 ,
+         QFA , QFA , DL_25 , Sf , DL_25 , WA , DL_25 , BPM , DL_25]*7 +\
+  [QDA , QDA , DL_25 , Sd , DL_25 , WA , DL_25 , BPM , DL_25 ,
+         QFA]
+
+SS1H1 = [QFS , DL_25 , Sf , DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
+         QDS , QDS , DL_25 , Sd , DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
+         QFS , QFS , DL_25 , Sf , DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
+         QDS]
+
+lattice = SS1H2+ARC1+SS2H1+SS2H2+ARC2+SS1H1
