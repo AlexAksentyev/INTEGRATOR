142ae61ea0350773e53be7ddf1d6e7f66fb1f8aa
Alex Aksentyev <alexaksentyev@gmail.com>
2017-09-20 11:25:18 +0200

    Syntactic correction + type check for dipole

--- CElement.py
+++ CElement.py
@@ -1,5 +1,5 @@
 import numpy as NP
-
+import collections as CLN
 
 
 
@@ -54,17 +54,18 @@ class MDipole(Element):
     and fCurve = 0
     """
     
-    _BField = None
+    _fBField = None
     
     def __init__(self, Length, R, BField):
         Element.__init__(self, 1/R, Length)
         self.setBField(BField)
         
     def setBField(self,BField):
-        self._BField = BField[:]
+        if not isinstance(BField, CLN.Sequence): BField = (0,BField,0) # by default B = By
+        self._fBField = BField[:]
         
     def getBField(self):
-        return self._BField[:]
+        return self._fBField[:]
     
     @classmethod    
     def computeBStrength(cls,particle, R):
@@ -76,7 +77,7 @@ class MDipole(Element):
         
         
     def BField(self, arg):
-        return self._BField
+        return self._fBField
   
 
 class Solenoid(MDipole):
@@ -112,15 +113,15 @@ class Wien(Element):
     The magnetic field definition is weird
     """
     
-    _R = None
-    _Volt = None
-    _BField = None
+    _fR = None
+    _fVolt = None
+    _fBField = None
     
     def __init__(self, Length, R, Hgap, Voltage, BField):
         Element.__init__(self, 1/R, Length)
-        self._R = [R, R - Hgap, R**2/(R-Hgap)]
-        self._Volt = Voltage
-        self._BField = BField
+        self._fR = [R, R - Hgap, R**2/(R-Hgap)]
+        self._fVolt = Voltage
+        self._fBField = BField
         
     @staticmethod
     def computeVoltage(particle, R, Hgap):
@@ -145,18 +146,18 @@ class Wien(Element):
         
     def EField(self, arg):
         x = arg[0]
-        Ex = -2*self._Volt/(NP.log(self._R[2]/self._R[1])*(self._R[0]+x))
+        Ex = -2*self._fVolt/(NP.log(self._fR[2]/self._fR[1])*(self._fR[0]+x))
         return (Ex, 0, 0)
     
     def BField(self, arg):        
-        return (0, self._BField, 0)
+        return (0, self._fBField, 0)
     
     def frontKick(self, particle):
         x=particle._fState[0]
-        R = self._R[0]
-        R1 = self._R[1]
-        R2 = self._R[2]
-        V = self._Volt
+        R = self._fR[0]
+        R1 = self._fR[1]
+        R2 = self._fR[2]
+        V = self._fVolt
         u = -V + 2*V*NP.log((R+x)/R1)/NP.log(R2/R1)
         Xk = particle.getState()
         Xk[5] -= u*1e-6/particle.fKinEn0
@@ -164,10 +165,10 @@ class Wien(Element):
         
     def rearKick(self, particle):
         x=particle._fState[0]
-        R = self._R[0]
-        R1 = self._R[1]
-        R2 = self._R[2]
-        V = self._Volt
+        R = self._fR[0]
+        R1 = self._fR[1]
+        R2 = self._fR[2]
+        V = self._fVolt
         u = -V + 2*V*NP.log((R+x)/R1)/NP.log(R2/R1)
         Xk = particle.getState()
         Xk[5] += u*1e-6/particle.fKinEn0
