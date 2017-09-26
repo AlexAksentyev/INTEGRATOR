0395eb24dc062670ae68e615350e4645574255eb
Alex Aksentyev <alexaksentyev@gmail.com>
2017-09-26 10:12:24 +0200

    there was confusion of the math function gamma, and the aux function gamma

--- PyDST_test.py
+++ PyDST_test.py
@@ -5,27 +5,30 @@ Created on Fri Sep 22 14:03:39 2017
 
 @author: alexa
 """
-import TParticle as PCL
+import CParticle as PCL
 import CElement as ENT
 import numpy as NP
 from matplotlib import pyplot as PLT
 import PyDSTool as DST
+from importlib import reload
 
+reload(PCL)
+reload(ENT)
 
 #%%
 L=20
 q = 1.602176462e-19
 #state = [1e-3, -1e-3, 0, -1e-3, 1e-3, 1e-4, 0, 0, 1, 0]
-state = [1e-3, -1e-3, 0, 1e-3, 0, 0, 0, 0, 1, 0]
+state = [1e-3, -1e-3, 0, 1e-4, 0, 0, 0, 0, 1, 0]
 names = ['x','y','time','px','py','dK','Sx','Sy','Ss','H']
 icdict = dict(zip(names,state))
 
-p = PCL.Particle(1800,270,-.14)
+p = PCL.Particle(state)
 el = ENT.MDipole(2,7.5,.46)
 
 xp = 'prime(px,px,py,0)'
 yp = 'prime(py,px,py,0)'
-tp = 'prime(hs(x),px,py,dK)/v(dK)'
+tp = 'prime(hs(x),px,py,dK)/vel(dK)'
 Hp = 'prime(hs(x),px,py,dK)'
 dKp = 'dKprime(px,py,Ex,Ey,Es)'
 pxp = '0'
@@ -44,20 +47,20 @@ DSargs.varspecs = {'x': xp, 'y': yp, 'time':tp, 'H':Hp,
 DSargs.ics = icdict
 DSargs.pars = {'Mass0':p.fMass0,'KinEn0':p.fKinEn0, 'P0c':p.Pc(p.fKinEn0),
                'G':p.fG, 'crv':el.fCurve, 
-               'clight': 2.99792458e8, 'q':q, 'm':q*1e6*p.fMass0/p.CLIGHT()**2}
+               'clight': p.CLIGHT(), 'q':q, 'm':q*1e6*p.fMass0/p.CLIGHT()**2}
 DSargs.fnspecs = {
         'KinEn':(['dK'], 'KinEn0*(1+dK)'),
         'prime':(['p','px','py','dK'],'p*Pc(dK)/Ps(dK,px,py)'),
         'hs':(['x'],'1+crv*x'),
         'Pc':(['dK'],'sqrt(pow(Mass0 + KinEn(dK),2) - pow(Mass0,2))'),
         'Ps':(['dK','px','py'],'sqrt(pow(Pc(dK),2)-pow(P0c*px,2)-pow(P0c*py,2))'),
-        'v':(['dK'],'beta(KinEn(dK))*clight'),
-        'gamma':(['dK'],'KinEn(dK) / Mass0 + 1'),
-        'beta':(['dK'], '.48'),#'sqrt(pow(gamma(dK),2)-1)/gamma(dK)'),
+        '_gamma':(['dK'],'(KinEn(dK) / Mass0) + 1'),
+        '_beta':(['dK'], 'sqrt(pow(_gamma(dK),2)-1)/_gamma(dK)'),#'sqrt(pow(gamma(dK),2)-1)/gamma(dK)'
+        'vel':(['dK'],'_beta(dK)*clight'),
         'dKprime':(['px','py','Ex','Ey','Es'],'(Ex*prime(px,px,py,0) +Ey*prime(py,px,py,0) + Es) * 1e-6/ KinEn0'),
         'gammap':(['dKp'],'dKp/Mass0'),
         'betap':(['dK','dKp'], '(dKp*pow(Mass0,2))/(pow(KinEn(dK)+Mass0,2)*sqrt(pow(KinEn(dK),2)+2*KinEn(dK)*Mass0))'),
-        'D':(['x','px','py','dK','By','Bx','Es'],'(q/(m*hs(x)))*(prime(px,px,py,0)*By-prime(py,px,py,0)*Bx+prime(hs(x),px,py,0)*Es/v(dK))-((gamma(dK)*v(dK))/(prime(hs(x),px,py,dK)*hs(x)))*3*crv*prime(px,px,py,0)')
+        'D':(['x','px','py','dK','By','Bx','Es'],'(q/(m*hs(x)))*(prime(px,px,py,0)*By-prime(py,px,py,0)*Bx+prime(hs(x),px,py,0)*Es/vel(dK))-((_gamma(dK)*vel(dK))/(prime(hs(x),px,py,dK)*hs(x)))*3*crv*prime(px,px,py,0)')
         }
 DSargs.ignorespecial = ['Ex','Ey','Es','Bx','By','Bs']
 DSargs.vfcodeinsert_start = """Ex,Ey,Es = ds.Element.EField([x,y,time])
