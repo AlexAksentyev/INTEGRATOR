6e83190753caf1ebe74735be90757616865e822a
Alex Aksentyev <alexaksentyev@gmail.com>
2017-10-06 11:11:56 +0200

    NTR

--- test.py
+++ test.py
@@ -35,28 +35,26 @@ mdip2 = ENT.MDipole(1.8,R,(B0/100, B0, 0))
 sol = ENT.Solenoid(1.8, .46)
 
 V = ENT.Wien.computeVoltage(p,R,.05)
-#B1 = ENT.Wien.computeBStrength(p,R,.05)
 wa = ENT.Wien(1.808,R,.05,V,B0)
 
 DIPS = list()
 for i in range(3):
     DIPS.append(ENT.MDipole(1.8,7.55,(.46/100,.46,0), 'Dipole_'+str(i)))
 
+Lat = LTC.Lattice([mdip], p)
 #%%
-
-Lat = LTC.Lattice(FODO, p)
 
 state = [1e-3, -1e-3, 0, 1e-3, -1e-3, 1e-4, 0, 0, 1, 0, 0, 0]
 names = ['x','y','ts','px','py','dK','Sx','Sy','Ss','H', 's', 'start']
 icdict = dict(zip(names,state))
 
 
-testname = 'test2'
+testname = 'test100'
 icdict.update({'start':'0'})
-Lat.fDSModel.compute(testname,ics=icdict,tdata=[0,35])
+Lat.fDSModel.compute(testname,ics=icdict,tdata=[0,100])
 pts = Lat.fDSModel.sample(testname)
 #%%
+PLT.plot(pts['t'], pts['Sy'], label='Sy')
 PLT.plot(pts['t'], pts['x'], label='x')
-PLT.plot(pts['t'], pts['px'], label='px')
 PLT.legend()
 PLT.xlabel('s')
