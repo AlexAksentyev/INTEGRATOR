318379d85d0670c05141a547c54eafa84fda1737
Alex Aksentyev <alexaksentyev@gmail.com>
2017-10-06 13:34:12 +0200

    NTR

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 from ggplot import ggplot, aes, geom_line, theme_bw, facet_wrap, facet_grid
-import pandas as PDS
+from importlib import reload
+
 import CParticle as PCL
 import CElement as ENT
-from importlib import reload
-import numpy as NP
 import CLattice as LTC
+import numpy as NP
+
 from matplotlib import pyplot as PLT
 
 reload(ENT)
@@ -13,16 +14,15 @@ reload(LTC)
 
 theme_bw()
 
-state = [1e-3, -1e-3, 0, 0, 0, 1e-4, 0, 0, 1, 0]
-StateList = [
-        [1e-3, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-        [0, 1e-3, 0, 0, .7, 0, 0, 0, 1, 0],
-        [0, 0, 0, 1e-4, 0, 0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 1e-4, 0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 0, 1e-4, 0, 0, 1, 0]
-        ]
-
 p = PCL.Particle()
+
+StateDict = {'X0':[0,0,0,0,0,0,0,0,1,0,0],
+             'X1':[1e-3,0,0,0,0,0,0,0,1,0,0],
+             'X2':[0,1e-3,0,0,0,0,0,0,1,0,0],
+             'X3':[0,0,0,1e-3,0,0,0,0,1,0,0],
+             'X4':[0,0,0,0,1e-3,0,0,0,1,0,0]}
+
+E = PCL.Ensemble(p, StateDict)
 
 #%%
 
@@ -30,10 +30,6 @@ FODO = [ENT.MQuad(5, .86, "QF1"), ENT.Drift(2.5,"O1") , ENT.MQuad(5, -.831, "QD1
 
 B0 = .46
 R = ENT.MDipole.computeRadius(p,B0)
-mdip = ENT.MDipole(1.8, R, B0)
-mdip2 = ENT.MDipole(1.8,R,(B0/100, B0, 0))
-sol = ENT.Solenoid(1.8, .46)
-
 V = ENT.Wien.computeVoltage(p,R,.05)
 wa = ENT.Wien(1.808,R,.05,V,B0)
 
@@ -41,20 +37,17 @@ DIPS = list()
 for i in range(3):
     DIPS.append(ENT.MDipole(1.8,7.55,(.46/100,.46,0), 'Dipole_'+str(i)))
 
-Lat = LTC.Lattice([mdip], p)
+Lat = LTC.Lattice(FODO, p)
 #%%
 
-state = [1e-3, -1e-3, 0, 1e-3, -1e-3, 1e-4, 0, 0, 1, 0, 0, 0]
-names = ['x','y','ts','px','py','dK','Sx','Sy','Ss','H', 's', 'start']
-icdict = dict(zip(names,state))
+Lat.track(E, 100)
 
-
-testname = 'test100'
-icdict.update({'start':'0'})
-Lat.fDSModel.compute(testname,ics=icdict,tdata=[0,100])
-pts = Lat.fDSModel.sample(testname)
+testpart = 'X3'
+pts = Lat.fDSModel.sample(testpart)
 #%%
-PLT.plot(pts['t'], pts['Sy'], label='Sy')
+PLT.plot(pts['t'], pts['y'], label='y')
 PLT.plot(pts['t'], pts['x'], label='x')
 PLT.legend()
 PLT.xlabel('s')
+PLT.title(testpart)
+#PLT.ylabel('y')
