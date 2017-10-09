ac068cf7d744984cc3c52521adacf78b9ee6dbdb
Alex Aksentyev <alexaksentyev@gmail.com>
2017-10-09 15:59:10 +0200

    tested auto-naming

--- ComTest.py
+++ ComTest.py
@@ -33,7 +33,7 @@ E = PCL.Ensemble(p, StateDict)
 
 #%%
 
-FODO = [ENT.MQuad(5, .86, "QF1"), ENT.Drift(2.5,"O1") , ENT.MQuad(5, -.831, "QD1"), ENT.Drift(2.5,"O2")]
+FODO = [ENT.MQuad(5, .86, "QF"), ENT.Drift(2.5,"O") , ENT.MQuad(5, -.831, "QD"), ENT.Drift(2.5,"O")]
 
 B0 = .46
 R = ENT.MDipole.computeRadius(p,B0)
@@ -42,7 +42,7 @@ wa = ENT.Wien(1.808,R,.05,V,B0)
 
 DIPS = list()
 for i in range(3):
-    DIPS.append(ENT.MDipole(1.8,7.55,(.46/100,.46,0), 'Dipole_'+str(i)))
+    DIPS.append(ENT.MDipole(1.8,7.55,(.46/100,.46,0)))
 
 Lat = LTC.Lattice(DIPS, p)
 #%%
