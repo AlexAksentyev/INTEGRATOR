891532444fe02adc1721f8312d3e9af7cb1ac872
Alex Aksentyev <alexaksentyev@gmail.com>
2017-10-09 15:16:35 +0200

    app test and language test

---
+++ ComTest.py
@@ -0,0 +1,61 @@
+from ggplot import ggplot, aes, geom_line, theme_bw, facet_wrap, facet_grid
+from importlib import reload
+
+import CParticle as PCL
+import CElement as ENT
+import CLattice as LTC
+import numpy as NP
+
+from matplotlib import pyplot as PLT
+
+reload(ENT)
+reload(PCL)
+reload(LTC)
+
+theme_bw()
+
+p = PCL.Particle()
+
+#%% form beam
+
+xs = NP.linspace(0, 5e-3, 2)
+ys = NP.linspace(0, 5e-3, 3)
+n = len(xs)*len(ys)
+
+StateDict=dict()
+i=0
+for x in xs:
+    for y in ys:
+        StateDict.update({"X"+str(i): [x,y,0,0,0,0,0,0,1,0,0]})
+        i += 1
+
+E = PCL.Ensemble(p, StateDict)
+
+#%%
+
+FODO = [ENT.MQuad(5, .86, "QF1"), ENT.Drift(2.5,"O1") , ENT.MQuad(5, -.831, "QD1"), ENT.Drift(2.5,"O2")]
+
+B0 = .46
+R = ENT.MDipole.computeRadius(p,B0)
+V = ENT.Wien.computeVoltage(p,R,.05)
+wa = ENT.Wien(1.808,R,.05,V,B0)
+
+DIPS = list()
+for i in range(3):
+    DIPS.append(ENT.MDipole(1.8,7.55,(.46/100,.46,0), 'Dipole_'+str(i)))
+
+Lat = LTC.Lattice(DIPS, p)
+#%%
+
+E.track(Lat,100,'2')
+Lat.track(E, 100,'2')
+
+testpart = 'X2'
+pts = E[testpart].sample()
+#%%
+PLT.plot(pts['px'], pts['py'], label='y')
+PLT.plot(pts['x'], pts['Sx'], label='x')
+PLT.legend()
+PLT.xlabel('s')
+PLT.title(testpart)
+#PLT.ylabel('y')
--- test.py
+++ test.py
@@ -1,60 +1,41 @@
-from ggplot import ggplot, aes, geom_line, theme_bw, facet_wrap, facet_grid
-from importlib import reload
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  9 14:53:02 2017
 
-import CParticle as PCL
-import CElement as ENT
-import CLattice as LTC
-import numpy as NP
+@author: alexa
+"""
 
-from matplotlib import pyplot as PLT
+class Base:
+    
+    def __init__(self, name):
+        self.fName = name
+        
+class DerivedA(Base):
+    fCount = 0
+    def __init__(self, name):
+        self.fName = name
+        DerivedA.fCount +=1
+        
+class DerivedB(Base):
+    fCount = 0
+    def __init__(self, name):
+        self.fName = name
+        DerivedB.fCount +=1
+        
 
-reload(ENT)
-reload(PCL)
-reload(LTC)
-
-theme_bw()
-
-p = PCL.Particle()
-
-#%% form beam
-
-xs = NP.linspace(0, 5e-3, 2)
-ys = NP.linspace(0, 5e-3, 3)
-n = len(xs)*len(ys)
-
-StateDict=dict()
-i=0
-for x in xs:
-    for y in ys:
-        StateDict.update({"X"+str(i): [x,y,0,0,0,0,0,0,1,0,0]})
-        i += 1
-
-E = PCL.Ensemble(p, StateDict)
-
-#%%
-
-FODO = [ENT.MQuad(5, .86, "QF1"), ENT.Drift(2.5,"O1") , ENT.MQuad(5, -.831, "QD1"), ENT.Drift(2.5,"O2")]
-
-B0 = .46
-R = ENT.MDipole.computeRadius(p,B0)
-V = ENT.Wien.computeVoltage(p,R,.05)
-wa = ENT.Wien(1.808,R,.05,V,B0)
-
-DIPS = list()
-for i in range(3):
-    DIPS.append(ENT.MDipole(1.8,7.55,(.46/100,.46,0), 'Dipole_'+str(i)))
-
-Lat = LTC.Lattice(DIPS, p)
-#%%
-
-Lat.track(E, 100,'2')
-
-testpart = 'X2'
-pts = E[testpart].sample()
-#%%
-PLT.plot(pts['px'], pts['py'], label='y')
-PLT.plot(pts['x'], pts['Sx'], label='x')
-PLT.legend()
-PLT.xlabel('s')
-PLT.title(testpart)
-#PLT.ylabel('y')
+if __name__ == '__main__':
+    
+    print("None: "+str(DerivedA.fCount))
+    print("None: "+str(DerivedB.fCount))
+    first = DerivedA("Johnny")
+    print("+A: "+str(DerivedA.fCount))
+    print("+A: "+str(DerivedB.fCount))
+    second = DerivedB("Mary")
+    print("+B: "+str(DerivedA.fCount))
+    print("+B: "+str(DerivedB.fCount))
+    third = DerivedA("Carl")
+    print("+A: "+str(DerivedA.fCount))
+    print("+A: "+str(DerivedB.fCount))
+    
+    
