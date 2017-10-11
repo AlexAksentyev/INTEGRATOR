adde1e2219862727b96ff801242eedb4b8e058b3
Alex Aksentyev <alexaksentyev@gmail.com>
2017-10-11 11:14:32 +0200

    NTR

--- test.py
+++ test.py
@@ -12,11 +12,11 @@ theme_bw()
 
 state = [5e-3, 0, 0, 0, 0, 0, 0, 0, 1, 0]
 StateList = [
-        [1e-3, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-        [0, 1e-3, 0, 0, 0, 0, 0, 0, 1, 0],
-        [3e-3, 3e-3, 0, 0, 0, 0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 1e-4, 0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 0, 1e-4, 0, 0, 1, 0]
+        [1e-3, 0, 0, 0, 0, 0, 0, 0, 1, 0,0],
+        [0, 1e-3, 0, 0, 0, 0, 0, 0, 1, 0,0],
+        [3e-3, 3e-3, 0, 0, 0, 0, 0, 0, 1, 0,0],
+        [0, 0, 0, 0, 1e-4, 0, 0, 0, 1, 0,0],
+        [0, 0, 0, 0, 0, 1e-4, 0, 0, 1, 0,0]
         ]
 
 p = PCL.Particle(state)
@@ -38,8 +38,8 @@ E = PCL.Ensemble.from_state(StateList)
 E.track(tLat,5)
     
 df = E.getDataFrame()
-df = PDS.melt(df, id_vars=['PID','t','H'])
+df = PDS.melt(df, id_vars=['PID','t','s'])
 #%%
-print(ggplot(df.loc[df['variable'].isin(['x','y'])&df['PID'].isin([2])],aes(x='H',y='value',color='variable'))
+print(ggplot(df.loc[df['variable'].isin(['x','y'])&df['PID'].isin([2])],aes(x='s',y='value',color='variable'))
     + geom_point() + geom_line() + theme_bw())
     
