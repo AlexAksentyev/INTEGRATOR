63d6c22909e46de2ed1125003dd8dee1f9338b65
Alex Aksentyev <alexaksentyev@gmail.com>
2017-10-20 17:32:44 +0200

    NTR

--- FODO_CC.py
+++ FODO_CC.py
@@ -26,26 +26,27 @@ def form_state_list(xint = (-5e-3,5e-3), yint=(-5e-3,5e-3), Nx = 3,Ny = 3):
 
 #%%
 
-#tLat = [ENT.MQuad(5e-2,-8.2,"QD"), ENT.Drift(25e-2), ENT.Drift(15e-2),
-#        ENT.Drift(25e-2), ENT.Drift(220e-2), ENT.Drift(25e-2),
-#        ENT.Drift(15e-2), ENT.Drift(25e-2), ENT.MQuad(5e-2,7.36,"QF"),
-#        ENT.MQuad(5e-2,7.36,"QF"), ENT.Drift(25e-2), ENT.Drift(15e-2),
-#        ENT.Drift(25e-2), ENT.Drift(220e-2), ENT.Drift(25e-2),
-#        ENT.Drift(15e-2), ENT.Drift(25e-2), ENT.MQuad(5e-2,-8.2,"QD"),
-#        ENT.MQuad(5e-2,-8.2,"QD"), ENT.Drift(25e-2), ENT.Drift(15e-2),
-#        ENT.Drift(25e-2), ENT.Drift(220e-2), ENT.Drift(25e-2),
-#        ENT.Drift(15e-2), ENT.Drift(25e-2), ENT.MQuad(5e-2,7.36,"QF")]
+tLat = [ENT.MQuad(5e-2,-8.2,"QD"), ENT.Drift(25e-2), ENT.Drift(15e-2),
+        ENT.Drift(25e-2), ENT.Drift(220e-2), ENT.Drift(25e-2),
+        ENT.Drift(15e-2), ENT.Drift(25e-2), ENT.MQuad(5e-2,7.36,"QF"),
+        ENT.MQuad(5e-2,7.36,"QF"), ENT.Drift(25e-2), ENT.Drift(15e-2),
+        ENT.Drift(25e-2), ENT.Drift(220e-2), ENT.Drift(25e-2),
+        ENT.Drift(15e-2), ENT.Drift(25e-2), ENT.MQuad(5e-2,-8.2,"QD"),
+        ENT.MQuad(5e-2,-8.2,"QD"), ENT.Drift(25e-2), ENT.Drift(15e-2),
+        ENT.Drift(25e-2), ENT.Drift(220e-2), ENT.Drift(25e-2),
+        ENT.Drift(15e-2), ENT.Drift(25e-2), ENT.MQuad(5e-2,7.36,"QF")]
     
 #tLat = [ENT.MDipole(5e-2,8,.46), ENT.MDipole(5e-2,8,-.46)]
 
 #tLat = [ENT.MQuad(5e-2,-8.2,"QD"), ENT.MQuad(5e-2,8.2,"QF")]
 
-tLat = LTC.Lattice([ENT.MQuad(5e-2,-8.2,"QD"), ENT.MQuad(5e-2,8.2,"QF")], PCL.Particle(),'Vode')
+tLat = LTC.Lattice([ENT.MDipole(25e-2,7,.46)], PCL.Particle(),'vode')
 StateList = form_state_list((1e-3,1e-3),(1e-3,1e-3),1,1)
 E = PCL.Ensemble.from_state(StateList)
+#E.set('X0',py=-1e-3)
 #%%
 
-tLat.track(E,10)
+tLat.track(E,1)
     
 #%%
 
@@ -57,7 +58,7 @@ def pos(data):
 df = E.getDataFrame()
 #df['Quad']=df.apply(pos, axis=1)
 df = PDS.melt(df, id_vars=['PID','s'])#, 'Turn','Element','Quad'])
-dat = df.loc[df['variable'].isin(['x','y'])&df['PID'].isin(['X0'])]
+dat = df.loc[df['variable'].isin(['px','py'])&df['PID'].isin(E.listNames())]
 print(ggplot(dat,aes(x='s',y='value',color='variable')) + 
      geom_line()  + theme_bw())
     
