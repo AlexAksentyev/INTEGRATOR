65f7c313b5c936893b1b3e0a6910e59f765967a7
Alex Aksentyev <alexaksentyev@gmail.com>
2017-10-23 15:13:43 +0200

    NTR

--- FODO_CC.py
+++ FODO_CC.py
@@ -40,13 +40,13 @@ tLat = [ENT.MQuad(5e-2,-8.2,"QD"), ENT.Drift(25e-2), ENT.Drift(15e-2),
 
 #tLat = [ENT.MQuad(5e-2,-8.2,"QD"), ENT.MQuad(5e-2,8.2,"QF")]
 
-tLat = LTC.Lattice([ENT.MDipole(25e-2,7,.46)], PCL.Particle(),'vode')
-StateList = form_state_list((1e-3,1e-3),(1e-3,1e-3),1,1)
+tLat = LTC.Lattice(tLat, PCL.Particle(),'vode')
+StateList = form_state_list((5e-3,1e-3),(5e-3,1e-3),1,1)
 E = PCL.Ensemble.from_state(StateList)
 #E.set('X0',py=-1e-3)
 #%%
 
-tLat.track(E,1)
+tLat.track(E,10)
     
 #%%
 
@@ -58,7 +58,7 @@ def pos(data):
 df = E.getDataFrame()
 #df['Quad']=df.apply(pos, axis=1)
 df = PDS.melt(df, id_vars=['PID','s'])#, 'Turn','Element','Quad'])
-dat = df.loc[df['variable'].isin(['px','py'])&df['PID'].isin(E.listNames())]
+dat = df.loc[df['variable'].isin(['x','y'])&df['PID'].isin(E.listNames())]
 print(ggplot(dat,aes(x='s',y='value',color='variable')) + 
      geom_line()  + theme_bw())
     
