af792d53e405bf300848a686864703bde6ce9222
AlexAksentyev <alexaksentyev@gmail.com>
2017-11-09 19:14:00 +0100

    Pointer to function

--- test.py
+++ test.py
@@ -14,35 +14,49 @@ from matplotlib import pyplot as PLT
 class Element:
     
     def __init__(self, parDict, Name = 'Element'):
-        MSpec = DST.LeafComponent(Name)
-        MSpec.compatibleGens = ('Vode_ODEsystem', 'Dopri_ODEsystem', 'Radau_ODEsystem')
-        MSpec.targetLangs = DST.targetLangs
+        self.__fModSpec = DST.LeafComponent(Name)
+        self.__fModSpec.compatibleGens = ('Vode_ODEsystem', 'Dopri_ODEsystem', 'Radau_ODEsystem')
+        self.__fModSpec.targetLangs = DST.targetLangs
     
         if any([e not in parDict.keys() for e in ['A','w','phi']]): raise Exception('Insufficient parameters')
         
         
         for n,v in parDict.items():
-            MSpec.add(DST.Par(str(v), n, domain=[-DST.Inf, DST.Inf]))
+            self.__fModSpec.add(DST.Par(str(v), n, domain=[-DST.Inf, DST.Inf]))
             
         m = DST.Par(1,'m',domain=[-DST.Inf, DST.Inf])
         k = DST.Par(1.13,'k',domain=[-DST.Inf, DST.Inf])
-        MSpec.add([m,k])
+        self.__fModSpec.add([m,k])
         
         
+#        x = DST.Var('x',domain=[-DST.Inf, DST.Inf])
+#        t = DST.Var('t',domain=[-DST.Inf, DST.Inf])
+        
+        f = DST.Fun('A*cos(w*t+phi)',['t'], 'force')
+        self.__fModSpec.add(f)
+        
+        self.addRHS()
+        
+#        x_RHS = DST.Var('y','x',specType='RHSfuncSpec',domain=[-DST.Inf, DST.Inf])
+#        y_RHS = DST.Var(-k/m*x + f(t)/m,'y',specType='RHSfuncSpec',domain=[-DST.Inf, DST.Inf])
+#    
+#        self.__fModSpec.add([x_RHS, y_RHS])
+        
+    def getModSpec(self): return self.__fModSpec
+    
+    def addRHS(self):
         x = DST.Var('x',domain=[-DST.Inf, DST.Inf])
         t = DST.Var('t',domain=[-DST.Inf, DST.Inf])
         
-        f = DST.Fun('A*cos(w*t+phi)',['t'], 'force')
-        MSpec.add(f)
+        f = self.__fModSpec['force']
+        if(id(f) == id(self.__fModSpec['force'])): print('Pointer')
+        k = self.__fModSpec['k']
+        m = self.__fModSpec['m']
         
         x_RHS = DST.Var('y','x',specType='RHSfuncSpec',domain=[-DST.Inf, DST.Inf])
         y_RHS = DST.Var(-k/m*x + f(t)/m,'y',specType='RHSfuncSpec',domain=[-DST.Inf, DST.Inf])
-    
-        MSpec.add([x_RHS, y_RHS])
-        self.__fModSpec = MSpec
         
-    def getModSpec(self): return self.__fModSpec
-        
+        self.__fModSpec.add([x_RHS, y_RHS])
         
     def getModel(self, targetGen='Vode', algparams=None):
         targetGen = targetGen.capitalize() + '_ODEsystem'
@@ -67,6 +81,8 @@ if __name__ is '__main__':
     
     e = Element(parDict, 'Vode_system')
     ms = e.getModSpec()
+    
+#%%
     m = e.getModel()
     m.compute('test1',ics={'x':0,'y':0},tdata=[0,10])
     pts = m.sample('test1')
