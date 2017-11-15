32ec7e82598c1057b81a0e6cd99097693de0e284
Alex Aksentyev <alexaksentyev@gmail.com>
2017-11-15 17:34:14 +0100

    running into NaN error at Ps

--- BNL.py
+++ BNL.py
@@ -6,13 +6,15 @@ Created on Mon Sep 18 16:59:23 2017
 @author: alexa
 """
 #%%
-from ggplot import ggplot, aes, geom_line, theme_bw, facet_wrap
 import pandas as PDS
 import CParticle as PCL
 import CElement as ENT
+import utilFunc as U
 from importlib import reload
 
-theme_bw()
+reload(ENT)
+reload(PCL)
+reload(U)
 
 # hardware parameters
 Lq = 5
@@ -21,16 +23,10 @@ Ls = .15
 GSFP = 0 
 GSDP = 0
 
-h = .05
-R = 9.207
-
+Lw = 361.55403e-2
+B = .082439761
+E = -120e5
 #%%
-# particle definition
-state0 = [1e-3, -1e-3, 0, 0, 0, 0, 0, 0, 1, 0]
-p = PCL.Particle(state0)
-
-#%%
-
 # lattice elements
 
 DL_25  = ENT.Drift(.25)
@@ -47,24 +43,21 @@ QFA = ENT.MQuad(Lq, 1.364)
 Sf = ENT.MSext(Ls, GSFP)
 Sd = ENT.MSext(Ls, GSDP)
 
-V = ENT.Wien.computeVoltage(p, R, h)
-B = ENT.Wien.computeBStrength(p, R, h)
-
-WA = ENT.Wien(1.808, 9.297, h, V, B)
+R3 = ENT.Wien(Lw,5e-2,PCL.Particle([0]),E,B)
 
 #%%
 # lattice definition
 
 SS1H2 = [QDS , DL_25 , DL_15 , DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
-         QFS , QFS , DL_25 , DL_15 , #  RF ,
+         QFS , QFS , DL_25 , DL_15 , ENT.Element(0,0) , #  RF ,
                                      DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
          QDS , QDS , DL_25 , DL_15 , DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
          QFS]
 
-ARC1 = [QFA , DL_25 , Sf , DL_25 , WA , DL_25 , BPM , DL_25] +\
- [QDA , QDA , DL_25 , Sd , DL_25 , WA , DL_25 , BPM , DL_25 ,
-         QFA , QFA , DL_25 , Sf , DL_25 , WA , DL_25 , BPM , DL_25]*7 +\
-  [QDA , QDA , DL_25 , Sd , DL_25 , WA , DL_25 , BPM , DL_25 ,
+ARC1 = [QFA , DL_25 , Sf , DL_25 , R3 , DL_25 , BPM , DL_25] +\
+ [QDA , QDA , DL_25 , Sd , DL_25 , R3 , DL_25 , BPM , DL_25 ,
+         QFA , QFA , DL_25 , Sf , DL_25 , R3 , DL_25 , BPM , DL_25]*7 +\
+  [QDA , QDA , DL_25 , Sd , DL_25 , R3 , DL_25 , BPM , DL_25 ,
          QFA]
 
 SS2H1 = [QFS , DL_25 , Sf , DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
@@ -76,10 +69,10 @@ SS2H2 = [QDS , DL_25 , DL_15 , DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
          QDS , QDS , DL_25 , DL_15 , DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
          QFS]
 
-ARC2 =  [QFA , DL_25 , Sf , DL_25 , WA , DL_25 , BPM , DL_25] +\
- [QDA , QDA , DL_25 , Sd , DL_25 , WA , DL_25 , BPM , DL_25 ,
-         QFA , QFA , DL_25 , Sf , DL_25 , WA , DL_25 , BPM , DL_25]*7 +\
-  [QDA , QDA , DL_25 , Sd , DL_25 , WA , DL_25 , BPM , DL_25 ,
+ARC2 =  [QFA , DL_25 , Sf , DL_25 , R3 , DL_25 , BPM , DL_25] +\
+ [QDA , QDA , DL_25 , Sd , DL_25 , R3 , DL_25 , BPM , DL_25 ,
+         QFA , QFA , DL_25 , Sf , DL_25 , R3 , DL_25 , BPM , DL_25]*7 +\
+  [QDA , QDA , DL_25 , Sd , DL_25 , R3 , DL_25 , BPM , DL_25 ,
          QFA]
 
 SS1H1 = [QFS , DL_25 , Sf , DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
@@ -90,4 +83,29 @@ SS1H1 = [QFS , DL_25 , Sf , DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
 lattice = SS1H2+ARC1+SS2H1+SS2H2+ARC2+SS1H1
 
 #%%
-# work code
+## prepping ensemble of states
+StateList = U.form_state_list((0e-3,0e-3),(-0e-3,0e-3),3,1)
+E = PCL.Ensemble.from_state(StateList)
+E.setReference(0)
+n = E.count()-1
+ddk = 2e-4/n
+for i in range(1,E.count()):
+    E[i].set(dK=3e-4-(i-1)*ddk)
+
+# inserting RF
+LRF = 5e-4
+E_RF = 15e5
+H_num = 50
+Acc_len = LRF + sum([e.fLength for e in lattice])
+ERF = ENT.ERF(LRF,E,Acc_len,EField=E_RF,H_number=H_num)
+
+
+lattice[12] = ERF
+
+
+#%%
+## tracking
+E.track(lattice,10,inner=True)
+
+Th,dK, p = E.plot()
+    
