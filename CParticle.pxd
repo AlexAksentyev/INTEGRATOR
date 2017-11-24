062b0fa5707f380dfcc59d00b6ec29d0f73d5a33
Alex Aksentyev <alexaksentyev@gmail.com>
2017-11-24 17:59:18 +0100

    test code

---
+++ CParticle.pxd
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 24 16:00:57 2017
+
+@author: alexa
+"""
+
--- test.py
+++ test.py
@@ -1,60 +1,110 @@
-import pyximport; pyximport.install(pyimport=True)
+import numpy as NP
 
-import CParticle as PCL
-import CElement as ENT
-import utilFunc as U
 
-from time import clock
 
+class Element:
+    
+    def __init__(self, Name, Length, force):
+        self.name = Name
+        self.length = Length
+        self.force = force
+
+
+
+class Particle:
+    
+    def __init__(self, mass, rigid):
+        self.mass = mass
+        self.rigid = rigid
+        
+    def __RHS(self, state, at, field):
+        x,y = state
+        
+        k = self.rigid
+        m = self.mass
+        
+        return [y, k/m*x + field.force(at)]
+    
+    def __getitem__(self, name):
+        return self.log[name]
+    
+    def plot(self, Ylab, Xlab='t',**):
+        from matplotlib import pyplot as PLT
+        
+        PLT.plot(self[Xlab], self[Ylab],**kwargs)
+        PLT.xlabel(Xlab)
+        PLT.ylabel(Ylab)
+
+    def track(self, state0, FldSeq , ntimes):
+        brks = 101
+        
+        names = ['START']+[e.name for e in FldSeq]
+        n = str(len(names[NP.argmax(names)]))
+        EType = 'U'+n
+        vartype = [('Turn',int),('Element',EType),('t', float), ('x', float), ('y', float)]
+        
+        self.__fLastPnt = -1
+        
+        nrow = ntimes*len(FldSeq)*brks
+        self.log = NP.recarray(nrow,dtype=vartype)
+        ind = 0
+        
+        t0 = 0
+        for n in range(1,ntimes+1):
+            for i in range(len(FldSeq)):
+                percent = int(ind/nrow*100)
+                if (percent%10==0):
+                    print('Complete {} %'.format(percent))
+                    
+                element = FldSeq[i]
+                t1 = t0 + element.length
+                at = NP.linspace(t0, t1, brks)
+                t0 = t1
+                
+                vals = odeint(self.__RHS, state0, at, args=(element,))
+                for k in range(brks):
+                    self.log[ind] = n,element.name, at[k], *vals[k]
+                    ind += 1
+                
+        print('Complete 100 %')
+        
+        
 #%%
-
-# hardware parameters
-Lq = 5
-Ls = .15
-
-GSFP = 0 
-GSDP = 0
-
-Lw = 361.55403e-2
-B = .082439761
-E = -120e5
+if __name__ is '__main__':
+    from scipy.integrate import odeint
+    
+    n = 10
+    elist = NP.empty(n, dtype=Element)
+    for i in range(10):
+        A = 10
+        w = 3
+        phi = NP.pi/9*i
+        L = 5
+        Name = 'EL_'+str(i)
+        f = lambda t: A*NP.cos(w*t+phi)
+        elist[i] = Element(Name, L, f)
+        
+    
+    p = Particle(10, 1.13)
+        
 #%%
-# lattice elements
-
-DL_25  = ENT.Drift(.25)
-DL_15 = ENT.Drift(.15)
-DL2_2 = ENT.Drift(2.2)
-BPM = ENT.Drift(15)
-
-QDS = ENT.MQuad(Lq, -.86)
-QFS = ENT.MQuad(Lq, .831)
-
-QDA = ENT.MQuad(Lq, -1.023)
-QFA = ENT.MQuad(Lq, 1.364)
-
-Sf = ENT.MSext(Ls, GSFP)
-Sd = ENT.MSext(Ls, GSDP)
-
-R3 = ENT.Wien(Lw,5e-2,PCL.Particle([0]),E,B)
-
-#%%
-# lattice definition
-
-SS1H2 = [QDS , DL_25 , DL_15 , DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
-         QFS , QFS , DL_25 , DL_15 , ENT.Element(0,0) , #  RF ,
-                                     DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
-         QDS , QDS , DL_25 , DL_15 , DL_25 , DL2_2 , DL_25 , BPM , DL_25 ,
-         QFS]
-
-
-StateList = U.form_state_list((0e-3,0e-3),(-0e-3,0e-3),3,1)
-E = PCL.Ensemble.from_state(StateList)
-E.setReference(0)
-n = E.count()-1
-ddk = 2e-4/n
-for i in range(1,E.count()):
-    E[i].set(dK=3e-4-(i-1)*ddk)
-
-start = clock()
-E.track(SS1H2,1000,inner=True)
-print('Time passed: {} sec'.format(clock()-start))
+    p.track([0,.1],elist, 10)
+    p.plot('x')
+    
+    
+    #%%
+    x = NP.arange(0,1e6,.1)
+    start = clock()
+    y = NP.sin(x)
+    ntime = clock() - start
+    print('Time passed {} sec'.format(ntime))
+    
+    start = clock()
+    y = NP.empty(len(x))
+    for i in range(len(x)):
+        y[i] = NP.sin(x[i])
+        
+    ntime_loop = clock() - start
+    print('Loop time {} sec'.format(ntime_loop))
+    
+    print('Ratio: {}'.format(ntime_loop/ntime))
