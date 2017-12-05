263e0e714a5652c08995d515dc4e2f34befc7e90
Alex Aksentyev <alexaksentyev@gmail.com>
2017-12-05 14:02:14 +0100

    separate elements

--- BNL.py
+++ BNL.py
@@ -16,78 +16,78 @@ reload(PCL)
 #%%
 # lattice elements
 
-OD1 = ENT.Drift(25e-2, "OD1")
-OD2 = ENT.Drift(25e-2, "OD2")
-ORE = ENT.Drift(2.17, "ORE")
-ORB = ENT.Drift(2.2, "ORB")
+OD1 = lambda : ENT.Drift(25e-2, "OD1")
+OD2 = lambda : ENT.Drift(25e-2, "OD2")
+ORE = lambda : ENT.Drift(2.17, "ORE")
+ORB = lambda : ENT.Drift(2.2, "ORB")
 
-QDA1 = ENT.MQuad(5e-2,-11.71, 'QDA1')
-QFA1 = ENT.MQuad(5e-2, 13.38, 'QFA1')
-QDA2 = ENT.MQuad(5e-2,-10.3,'QDA2')
-QFA2 = ENT.MQuad(5e-2, 10.11,'QFA2')
+QDA1 = lambda : ENT.MQuad(5e-2,-11.71, 'QDA1')
+QFA1 = lambda : ENT.MQuad(5e-2, 13.38, 'QFA1')
+QDA2 = lambda : ENT.MQuad(5e-2,-10.3,'QDA2')
+QFA2 = lambda : ENT.MQuad(5e-2, 10.11,'QFA2')
 
 
-OSF = ENT.MSext(15e-2,0,"OSF")
-OSD = ENT.MSext(15e-2,0,"OSD")
+OSF = lambda : ENT.MSext(15e-2,0,"OSF")
+OSD = lambda : ENT.MSext(15e-2,0,"OSD")
 
-SDP = ENT.MSext(15e-2,3.39598,"SDP")
-SFP = ENT.MSext(15e-2,2.76958,"SFP")
-SDN = ENT.MSext(15e-2,3.79311,"SDN")
-SFN = ENT.MSext(15e-2,2.09837,"SFN")
+SDP = lambda : ENT.MSext(15e-2,3.39598,"SDP")
+SFP = lambda : ENT.MSext(15e-2,2.76958,"SFP")
+SDN = lambda : ENT.MSext(15e-2,3.79311,"SDN")
+SFN = lambda : ENT.MSext(15e-2,2.09837,"SFN")
 
-BDA = ENT.MDipole(182.02463e-2, PCL.Particle(), BField=1.5,Name="BDA")
+BDA = lambda : ENT.MDipole(182.02463e-2, PCL.Particle(), BField=1.5,Name="BDA")
 
-BPM = ENT.Drift(15e-2,"BPM")
+BPM = lambda : ENT.Drift(15e-2,"BPM")
 
-R3 = ENT.Wien(361.55403e-2,5e-2,PCL.Particle(),-120e5,.082439761)
+R3 = lambda : ENT.Wien(361.55403e-2,5e-2,PCL.Particle(),-120e5,.082439761, Name="R3")
 
 #%%
 # lattice definition
 
-SSb1H2 = [QDA2, OD1, OSD, OD2, ORB, OD2, BPM, OD1, QFA2,
-        QFA2, OD1, OSF, OD2, ORB, OD2, BPM, OD1, QDA2,
-        QDA2, OD1, OSD, OD2, ORB, OD2, BPM, OD1, QFA2]
+SSb1H2 = [QDA2(), OD1(), OSD(), OD2(), ORB(), OD2(), BPM(), OD1(), QFA2(),
+        QFA2(), OD1(), OSF(), OD2(), ORB(), OD2(), BPM(), OD1(), QDA2(),
+        QDA2(), OD1(), OSD(), OD2(), ORB(), OD2(), BPM(), OD1(), QFA2()]
     
-ARCb1H2 = [QFA1, OD1, OSF, OD2, BDA, OD2, BPM, OD1, QDA1,
-        QDA1, OD1, SDP, OD2, BDA, OD2, BPM, OD1, QFA1]
+ARCb1H2 = [QFA1(), OD1(), OSF(), OD2(), BDA(), OD2(), BPM(), OD1(), QDA1(),
+        QDA1(), OD1(), SDP(), OD2(), BDA(), OD2(), BPM(), OD1(), QFA1()]
 
-SSe1H1 = [QFA2, OD1, SFP, OD2, R3, OD2, BPM, OD1, QDA2,
-         QDA2, OD1, SDP, OD2, R3, OD2, BPM, OD1, QFA2,
-         QFA2, OD1, SFP, OD2, R3, OD2, BPM, OD1, QDA2,
-         QDA2, OD1, SDN, OD2, R3, OD2, BPM, OD1, QFA2]
+SSe1H1 = [QFA2(), OD1(), SFP(), OD2(), R3(), OD2(), BPM(), OD1(), QDA2(),
+         QDA2(), OD1(), SDP(), OD2(), R3(), OD2(), BPM(), OD1(), QFA2(),
+         QFA2(), OD1(), SFP(), OD2(), R3(), OD2(), BPM(), OD1(), QDA2(),
+         QDA2(), OD1(), SDN(), OD2(), R3(), OD2(), BPM(), OD1(), QFA2()]
 
-SSe1H2 = [QFA2, OD1, SFN, OD2, R3, OD2, BPM, OD1, QDA2,
-         QDA2, OD1, SDN, OD2, R3, OD2, BPM, OD1, QFA2,
-         QFA2, OD1, SFP, OD2, R3, OD2, BPM, OD1, QDA2,
-         QDA2, OD1, SDP, OD2, R3, OD2, BPM, OD1, QFA2]
+SSe1H2 = [QFA2(), OD1(), SFN(), OD2(), R3(), OD2(), BPM(), OD1(), QDA2(),
+         QDA2(), OD1(), SDN(), OD2(), R3(), OD2(), BPM(), OD1(), QFA2(),
+         QFA2(), OD1(), SFP(), OD2(), R3(), OD2(), BPM(), OD1(), QDA2(),
+         QDA2(), OD1(), SDP(), OD2(), R3(), OD2(), BPM(), OD1(), QFA2()]
 
-ARCb2H1 = [QFA1, OD1, SFP, OD2, BDA, OD2, BPM, OD1, QDA1,
-        QDA1, OD1, SDP, OD2, BDA, OD2, BPM, OD1, QFA1]
+ARCb2H1 = [QFA1(), OD1(), SFP(), OD2(), BDA(), OD2(), BPM(), OD1(), QDA1(),
+        QDA1(), OD1(), SDP(), OD2(), BDA(), OD2(), BPM(), OD1(), QFA1()]
 
-SSe2H1 = [QFA2, OD1, SFP, OD2, ORB, OD2, BPM, OD1, QDA2,
-         QDA2, OD1, SDP, OD2, ORB, OD2, BPM, OD1, QFA2,
-         QFA2, OD1, SFP, OD2, ORB, OD2, BPM, OD1, QDA2]
+SSe2H1 = [QFA2(), OD1(), SFP(), OD2(), ORB(), OD2(), BPM(), OD1(), QDA2(),
+         QDA2(), OD1(), SDP(), OD2(), ORB(), OD2(), BPM(), OD1(), QFA2(),
+         QFA2(), OD1(), SFP(), OD2(), ORB(), OD2(), BPM(), OD1(), QDA2()]
 
-SSe2H2 = [QDA2, OD1, OSD, OD2, ORB, OD2, BPM, OD1, QFA2,
-         QFA2, OD1, OSF, OD2, ORB, OD2, BPM, OD1, QDA2,
-         QDA2, OD1, OSD, OD2, ORB, OD2, BPM, OD1, QFA2]
+SSe2H2 = [QDA2(), OD1(), OSD(), OD2(), ORB(), OD2(), BPM(), OD1(), QFA2(),
+         QFA2(), OD1(), OSF(), OD2(), ORB(), OD2(), BPM(), OD1(), QDA2(),
+         QDA2(), OD1(), OSD(), OD2(), ORB(), OD2(), BPM(), OD1(), QFA2()]
 
-SSb2H1 = [QFA2, OD1, SFP, OD2, R3, OD2, BPM, OD1, QDA2,
-         QDA2, OD1, SDP, OD2, R3, OD2, BPM, OD1, QFA2,
-         QFA2, OD1, SFP, OD2, R3, OD2, BPM, OD1, QDA2,
-         QDA2, OD1, SDN, OD2, R3, OD2, BPM, OD1, QFA2]
+SSb2H1 = [QFA2(), OD1(), SFP(), OD2(), R3(), OD2(), BPM(), OD1(), QDA2(),
+         QDA2(), OD1(), SDP(), OD2(), R3(), OD2(), BPM(), OD1(), QFA2(),
+         QFA2(), OD1(), SFP(), OD2(), R3(), OD2(), BPM(), OD1(), QDA2(),
+         QDA2(), OD1(), SDN(), OD2(), R3(), OD2(), BPM(), OD1(), QFA2()]
 
-SSb2H2 = [QFA2, OD1, SFN, OD2, R3, OD2, BPM, OD1, QDA2,
-         QDA2, OD1, SDN, OD2, R3, OD2, BPM, OD1, QFA2,
-         QFA2, OD1, SFP, OD2, R3, OD2, BPM, OD1, QDA2,
-         QDA2, OD1, SDP, OD2, R3, OD2, BPM, OD1, QFA2]
+SSb2H2 = [QFA2(), OD1(), SFN(), OD2(), R3(), OD2(), BPM(), OD1(), QDA2(),
+         QDA2(), OD1(), SDN(), OD2(), R3(), OD2(), BPM(), OD1(), QFA2(),
+         QFA2(), OD1(), SFP(), OD2(), R3(), OD2(), BPM(), OD1(), QDA2(),
+         QDA2(), OD1(), SDP(), OD2(), R3(), OD2(), BPM(), OD1(), QFA2()]
 
-ARCb1H1 = [QFA1, OD1, SFP, OD2, BDA, OD2, BPM, OD1, QDA1,
-        QDA1, OD1, SDP, OD2, BDA, OD2, BPM, OD1, QFA1]
+ARCb1H1 = [QFA1(), OD1(), SFP(), OD2(), BDA(), OD2(), BPM(), OD1(), QDA1(),
+        QDA1(), OD1(), SDP(), OD2(), BDA(), OD2(), BPM(), OD1(), QFA1()]
 
-SSb1H1 = [QFA2, OD1, SFP, OD2, ORB, OD2, BPM, OD1, QDA2,
-        QDA2, OD1, SDP, OD2, ORB, OD2, BPM, OD1, QFA2,
-        QFA2, OD1, SFP, OD2, ORB, OD2, BPM, OD1, QDA2]
+SSb1H1 = [QFA2(), OD1(), SFP(), OD2(), ORB(), OD2(), BPM(), OD1(), QDA2(),
+        QDA2(), OD1(), SDP(), OD2(), ORB(), OD2(), BPM(), OD1(), QFA2(),
+        QFA2(), OD1(), SFP(), OD2(), ORB(), OD2(), BPM(), OD1(), QDA2()]
 
 QFS = SSb1H2 + ARCb1H2 + SSe1H1 + SSe1H2 + \
     ARCb2H1 + SSe2H1 + SSe2H2 + ARCb1H2 + \
