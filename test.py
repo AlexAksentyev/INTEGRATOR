Corrected spin xs rotation to not affect Sy precession

test.py
@@ -1,21 +1,36 @@
 import numpy as np
 import math
+from pandas import DataFrame
+
+rad = np.deg2rad
+deg = np.rad2deg
 
 pi = np.pi
-R = lambda x: np.array([[math.cos(x) , -math.sin(x)], [math.sin(x), math.cos(x)]])
+R = lambda x: np.array([[np.cos(rad(x)) , -np.sin(rad(x))], [np.sin(rad(x)), np.cos(rad(x))]])
 s0 = np.array([0, 1])
-s1 = R(.3).dot(s0)
-s1[0] += .02
-s2 = R(pi/2-.23).dot(s0)
-s3 = R(pi-.05).dot(s0)
-s4 = R(-pi + .4).dot(s0)
-s5 = R(-pi/2 + .24).dot(s0)
+
+arg = -np.array([45, 135, 225, 315, -180])
+s1 = R(arg[0]).dot(s0)
+s2 = R(arg[1]).dot(s0)
+s3 = R(arg[2]).dot(s0)
+s4 = R(arg[3]).dot(s0)
+s5 = R(arg[4]).dot(s0)
 s = np.array([s1,s2,s3,s4,s5])
 
 s00 = np.array([s0,s0,s0,s0,s0])
 cos_psy = s.dot(s0)
 sin_phi = np.cross(s, s00)
-# sin_phi *= np.sign(cos_psy)
+
 for i, s_i in enumerate(s):
-    Ry = np.array([[cos_psy[i], -sin_phi[i]], [sin_phi[i], cos_psy[i]]])
+    sc = np.sign(cos_psy[i])
+    c = sc*cos_psy[i]
+    s = sc*sin_phi[i]
+    Ry = np.array([[c, -s], [s, c]])
+    #print((s_i, Ry.dot(Ry.dot(s_i))))
     print(Ry.dot(s_i))
+
+arg_cos = deg(np.arccos(cos_psy))
+arg_sin = deg(np.arcsin(sin_phi))
+cw = np.sign(cos_psy*sin_phi)
+print(DataFrame({'Atheta':arg, #'Ctheta':arg_cos, 'Stheta':arg_sin,
+                 'cos':cos_psy, 'sin':sin_phi, 'Ttheta':cw}) )

tracker.py
@@ -39,9 +39,10 @@ def rotate_spin(state, S0_xz):
     # computing the angles between S_xz and S0_xz
     sin_phi = np.cross(S_xz_u.T, S0_xz.T)
     cos_psy = S_xz_u.T.dot(S0_xz[:, 0]) # REFERENCE PARTICLE
+    sc = np.sign(cos_psy)
     # arrgegate angle: is reference particle's
-    sin_phi = sin_phi[0]
-    cos_psy = cos_psy[0]
+    sin_phi = sin_phi[0]*sc[0]
+    cos_psy = cos_psy[0]*sc[0]
     # sin_psy = np.sqrt(1 - cos_psy**2)*np.sign(sin_phi)
     # rotate the spins by the angle of the reference particle's spin
     Ry = np.array([[cos_psy, -sin_phi], [sin_phi, cos_psy]])
@@ -192,7 +193,7 @@ class Tracker:
                 print('NAN error: Element {}, turn {}, log index {}'.format(element.name, current_turn, log_index))
                 raise StopTracking('Stopping tracking due to a ValueError in _run_turn')
             # orthogonize_spin(state)
-            rotate_spin(state, self._S0_xz)
+            # rotate_spin(state, self._S0_xz)
         # end element loop
 
         return state, log_index
@@ -268,7 +269,7 @@ class Tracker:
                     self.log.write_file(file_handle, old_ind, log_ind)
                     return self.log
 
-                # rotate_spin(state, S0_xz) 
+                rotate_spin(state, S0_xz) 
 
                 if (turn-old_turn)%ncut == 0:
                     print('turn {}, writing data ...'.format(turn))