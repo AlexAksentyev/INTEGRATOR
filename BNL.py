Tracker: corrected spin orthogonalization, adjusted rotation

BNL.py
@@ -163,24 +163,10 @@ if __name__ == '__main__':
     lattice.plot_segment('OUT',log, 'Sy', 'Turn', pids=[pid], new_plot=False)
     plt.ylabel('Sx (blue), Sy (orange) for pid {}'.format(pid)); plt.show()
 
-    log.plot('Sz','Sx',pids=[0], marker='.'); plt.show()
+    # log.plot('Sz','Sx',pids=[0], marker='.'); plt.show()
 
     #%%
-    ## test of rotation matrix
-    pi = np.pi
-    R = lambda x: np.array([[math.cos(x) , -math.sin(x)], [math.sin(x), math.cos(x)]])
-    s0 = np.array([0, 1])
-    s1 = R(.3).dot(s0)
-    s1[0] += .02
-    s2 = R(pi/2-.23).dot(s0)
-    s3 = R(pi-.05).dot(s0)
-    s4 = R(-pi + .4).dot(s0)
-    s5 = R(-pi/2 + .24).dot(s0)
-    s = np.array([s1,s2,s3,s4,s5])
-
-    s00 = np.array([s0,s0,s0,s0,s0])
-    sin_phi = np.cross(s, s00)
-    cos_psy = s.dot(s0)
-    for i, s_i in enumerate(s):
-        Ry = np.array([[cos_psy[i], -sin_phi[i]], [sin_phi[i], cos_psy[i]]])
-        print(Ry.dot(s_i))
+    Sx, Sy, Sz = log['Sx'], log['Sy'], log['Sz']
+    S = np.array([Sx, Sy, Sz])
+    norm_S = np.linalg.norm(S, axis=0)
+    print(np.any(np.abs(norm_S[-10:-1,:] - 1) > 1e-11))

tracker.py
@@ -23,11 +23,11 @@ reload(rhs)
 
 def orthogonize_spin(state):
     ## orthogonality correction
-    ###### implementation leads to Sy staying at 1 (w/o x-s rotation)
     Sx_i, Sy_i, Sz_i = rhs.index(state, 'Sx', 'Sy', 'Sz')
+    sSs = np.sign(state[Sz_i])
     Ss2 = np.ones_like(Sx_i) - state[Sx_i]**2 - state[Sy_i]**2
     if np.all(Ss2 > 0):
-        state[Sz_i] = np.sqrt(Ss2)
+        state[Sz_i] = sSs*np.sqrt(Ss2)
     else:
         state[Sz_i] = np.zeros_like(Sx_i)
 
@@ -35,7 +35,14 @@ def rotate_spin(state, S0_xz):
     ## turn Sx back to its initial value *#!
     Sx_i, Sz_i = rhs.index(state, 'Sx', 'Sz')
     S_xz = np.array([state[Sx_i], state[Sz_i]])
-    S_xz_u = S_xz/norm(S_xz, axis=0) # |S0_xz| = 1 by assertion (above) 
+    norm_S_xz = norm(S_xz, axis=0)
+    # if np.any(norm_S_xz) == 0:
+    #     return
+    # catching those vectors whose norm is 0
+    # if the norm is 0, the vector is [0,0] anyway,
+    # so just set norm to 1
+    norm_S_xz = np.array([1 if x==0 else x for x in norm_S_xz])
+    S_xz_u = S_xz/norm_S_xz # |S0_xz| = 1 by assertion (above) 
     # computing the angles between S_xz and S0_xz
     sin_phi = np.cross(S_xz_u.T, S0_xz.T)
     cos_psy = S_xz_u.T.dot(S0_xz[:, 0]) # REFERENCE PARTICLE
@@ -192,8 +199,8 @@ class Tracker:
             except ValueError:
                 print('NAN error: Element {}, turn {}, log index {}'.format(element.name, current_turn, log_index))
                 raise StopTracking('Stopping tracking due to a ValueError in _run_turn')
-            # orthogonize_spin(state)
-            # rotate_spin(state, self._S0_xz)
+            rotate_spin(state, self._S0_xz)
+            orthogonize_spin(state)
         # end element loop
 
         return state, log_index
@@ -269,7 +276,7 @@ class Tracker:
                     self.log.write_file(file_handle, old_ind, log_ind)
                     return self.log
 
-                rotate_spin(state, S0_xz) 
+                # rotate_spin(state, S0_xz) 
 
                 if (turn-old_turn)%ncut == 0:
                     print('turn {}, writing data ...'.format(turn))