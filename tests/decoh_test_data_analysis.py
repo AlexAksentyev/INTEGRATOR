a1419c6cee6d5c31a999f6cd09914ec127e9c51e
Alex Aksentyev <alexaksentyev@gmail.com>
2018-04-18 14:14:13 +0200

    plot style changed for paper article

--- tests/decoh_test_data_analysis.py
+++ tests/decoh_test_data_analysis.py
@@ -1,13 +1,14 @@
 from tables import open_file
 import matplotlib.pyplot as plt
+from scipy.stats import linregress
 
 def get_hist(data, x_lab, title):
     plt.figure()
     plt.title(title)
     ax = plt.gca()
     ref_val = data[0]
-    ax.hist(data)
-    plt.axvline(ref_val, color='red')
+    ax.hist(data, histtype='step', color='black')
+    plt.axvline(ref_val, linestyle = 'dashed', color='black')
     ax.set_xlabel(x_lab)
     ax.set_ylabel('Counts')
     ax.get_xaxis().get_major_formatter().set_useOffset(False)
@@ -27,26 +28,31 @@ tilt_dK_means = tilt_dK.mean(axis=1)
 
 
 plt.rc('text', usetex=True)
-font = {'family' : 'normal',
-        'weight' : 'bold',
-        'size'   : 16}
+font = {'family' : 'sans-serif',
+        'weight' : 'heavy',
+        'size'   : 24}
 
 plt.rc('font', **font)
 
-plt.plot(tilt_dK_means, Wy_dK[:,0], '.')
+slp, icpt, r2, p_val, std_err = linregress(tilt_dK_means, Wx_dK[:, 0])
+plt.plot(tilt_dK_means, Wx_dK[:,0], '.', color='black')
+plt.table()
 plt.xlabel('Mean tilt angle, $\Theta$ [rad]')
-plt.ylabel('Reference horizontal spin precession frequency, $\Omega_y$ [rad/sec]')
+plt.ylabel('Reference vertical spin precession frequency, $\Omega_x$ [rad/sec]')
 
 i = tilt_dK_means.argmax()
 tilt = tilt_dK_means[i]
-get_hist(Wx_dK[i], r'$\Omega_x$ [rad]', r'$\Omega_x$ histogram for mean tilt angle {:4.2e} rad'.format(tilt))
-get_hist(Wy_dK[i], r'$\Omega_y$ [rad]', r'$\Omega_y$ histogram for mean tilt angle {:4.2e} rad'.format(tilt))
-i = tilt_dK_means.argmin()
-tilt = tilt_dK_means[i]
-get_hist(Wx_dK[i], r'$\Omega_x$ [rad]', r'$\Omega_x$ histogram for mean tilt angle {:4.2e} rad'.format(tilt))
+get_hist(Wx_dK[i], r'$\Omega_x$ [rad]', '' # r'$\Omega_x$ histogram for mean tilt angle {:4.2e} rad'.format(tilt)
+)
+get_hist(Wy_dK[i], r'$\Omega_y$ [rad]', '' # r'$\Omega_y$ histogram for mean tilt angle {:4.2e} rad'.format(tilt)
+)
+# i = tilt_dK_means.argmin()
+# tilt = tilt_dK_means[i]
+# get_hist(Wx_dK[i], r'$\Omega_x$ [rad]', r'$\Omega_x$ histogram for mean tilt angle {:4.2e} rad'.format(tilt))
 ## didn't include Wy because it's the same distribution for all tilts
 
 plt.plot(tilt_dK_means, Wy_dK[:, 0], '.')
 plt.xlabel('Mean tilt angle, $\Theta$ [rad]')
 plt.ylabel('Reference horizontal spin precession frequency, $\Omega_y$ [rad/sec]')
 
+plt.show()
