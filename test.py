ed886927ec0f3eabc5f69a453780df4bbce51ba2
Alex Aksentyev <alexaksentyev@gmail.com>
2018-04-26 14:10:14 +0200

    NTR

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from particle import Particle, EZERO, CLIGHT
 from plog import PLog
 from element import Drift, MQuad, RF
 from state_list import StateList
+from time import clock
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,25 +13,30 @@ F = MQuad(p, 25e-2, 8.6)
 D = MQuad(p, 25e-2, -8.11)
 RF = RF(p, 25e-2*3, 75000)
 
-state = StateList(x = 1e-3, d = [-.5e-4, 1e-4]).array
+state = StateList(x = [-1e-3, 1e-3], d = [-.5e-4, 1e-4]).array
 
 Om = O.M
 Fm = F.M
 Dm = D.M
 RFm = RF.M
-FODOm = RFm.dot(Om.dot(Dm.dot(Om.dot(Fm))))
+FODOm = RFm.dot(Dm.dot(Om.dot(Fm.dot(Om))))
 
-n_trn = int(10e3)
-n_rec = int(n_trn/1000)
+n_trn = int(10e5)
+n_rec = int(1e3)
+n_skip = int(n_trn/n_rec)
 log = PLog(state, n_rec) # save every thousandth phase picture
 ind = 0
+
+t0=clock()
 for i in range(1, n_trn+1):
     state = FODOm.dot(state).A
-    if i%1000 == 0:
+    if i%n_skip == 0:
         log[ind] = ((i,), state)
         ind += 1
 
-print("test success!")
+t1=clock()
+print("Run time: {} secs for {} turns".format(t1-t0, n_trn))
+print("{} sec/turn".format((t1-t0)/n_trn))
 
 pcl = log[:, :]
 plt.plot(pcl['z'], pcl['d'], '--.', markersize=.5); plt.show()
