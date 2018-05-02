a058e9f4fdf1d1ee7cc93fb24e6a3c4afce835af
Alex Aksentyev <alexaksentyev@gmail.com>
2018-05-02 13:22:20 +0200

    UPDATE: track function reduced loop iterations

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from particle import Particle, EZERO, CLIGHT
 from plog import PLog
-from element import Drift, MQuad, RF, MDipoleSect
+from element import Drift, MQuad, RF, MDipoleSect, CylWien
 from state_list import StateList
 from time import clock
 
@@ -11,14 +11,13 @@ def track(state, transfer_map, n_trn, n_rec = None):
     n_trn = int(n_trn)
     n_rec = int(n_rec) if n_rec is not None else n_trn
     n_skip = int(n_trn/n_rec)
-    log = PLog(state, n_rec) # save every thousandth phase picture
-    ind = 0
+    log = PLog(state, n_rec)
 
-    for i in range(1, n_trn+1):
-        state = transfer_map*state # state is matrix
-        if i%n_skip == 0:
-            log[ind] = ((i,), state.A) # retrieve array
-            ind += 1
+    TM_n = transfer_map**n_skip
+
+    for i in range(1, n_rec):
+        state = TM_n*state # state is matrix
+        log[i] = ((i*n_skip,), state.A) # retrieve array
 
     return log
 
@@ -30,6 +29,8 @@ RF = RF(p, 25e-2*3, 75000)
 
 DIP = MDipoleSect(p, 25e-2, 1)
 
+CWF = CylWien(p, 361e-2, 120e-5, .46)
+
 
 Om = O.M
 Fm = F.M
@@ -39,9 +40,10 @@ OFODORm = RFm*Om*Dm*Om*Fm*Om
 
 state = StateList(x = [-1e-3, 1e-3], d = [-.5e-4, 1e-4]).array
 
-log = track(state, OFODORm, 1e3, 100)
+lfodo = track(state, OFODORm, 1e6, 100)
 
-pcl = log[:, 3]
-plt.plot(pcl['y'], pcl['py'], '--.', markersize=.5); plt.show()
+pcl = lfodo[:, :]
+# plt.plot(pcl['z'], pcl['d'], '--.', markersize=.5); plt.show()
     
-log_dip = track(state, DIP.M, 100)
+ldip = track(state, DIP.M, 100)
+lcwf = track(state, CWF.M*O.M*O.M*RF.M*O.M, 100e3, 1000) 
