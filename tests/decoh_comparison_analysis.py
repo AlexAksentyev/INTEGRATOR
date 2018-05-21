cb831a36992c1c542699b29879761163cb767a62
AlexAksentyev_Mac <alexaksentyev@gmail.com>
2018-05-21 22:08:51 +0300

    update for MEPhI report

--- tests/decoh_comparison_analysis.py
+++ tests/decoh_comparison_analysis.py
@@ -1,6 +1,3 @@
-f0x, f1x, l0, l1 = fit_result['x']
-f0y, _ = analysis(l0, f0x['ini'], 'Sy')
-f1y, _ = analysis(l1, f1x['ini'], 'Sy')
 
 ##
 def plot_hists():
@@ -18,31 +15,40 @@ def plot_hists():
 
 def one_one(data, ax=None):
     if ax is None:
-        plt.plot(data, data, '--r')
+        plt.plot(data, data, '--', color='gray', linewidth=1)
     else:
-        ax.plot(data, data, '--r')
+        ax.plot(data, data, '--', color='gray', linewidth=1)
 
-def cross_plot():
+def cross_plot(fit_res, dist_var):
     def sub(axis, x, y0, y1, title):
         axis.plot(x, y0, '.b', label='no tilt')
         axis.plot(x, y1, '.r', label='w/tilt')
         axis.set_title(title)
-        axis.set_xlabel('initial distribution')
-        axis.set_ylabel('frequency')
+        axis.set_xlabel('initial distribution, {}/std({})'.format(dist_var, dist_var))
+        axis.set_ylabel('frequency, rad/sec')
+        axis.legend()
         
     def cross(axis, x, y, title):
         axis.plot(x, y, '.b')
         one_one(x, axis)
         axis.set_title(title)
-        axis.set_xlabel('no-tilt frequency')
-        axis.set_ylabel('tilted frequency')
+        axis.set_xlabel('no-tilt frequency, rad/sec')
+        axis.set_ylabel('tilted frequency, rad/sec')
+        axis.legend()
 
     tof = 1e-6
+    
+    f0x, f1x, l0, l1 = fit_res[dist_var]
+    f0y, _ = analysis(l0, f0x['ini'], 'Sy')
+    f1y, _ = analysis(l1, f1x['ini'], 'Sy')
+    sd_x = f0x['ini'].std()
+    sd_y = f0y['ini'].std()
+
     f, ((ax0, ax1),(ax2, ax3)) = plt.subplots(2,2)
-    sub(ax0, f0x['ini'], f0x['slp']/tof, f1x['slp']/tof, 'x-plane')
-    cross(ax1, f0x['slp']/tof, f1x['slp']/tof, 'x-plane')
-    sub(ax2, f0y['ini'], f0y['slp']/tof, f1y['slp']/tof, 'y-plane')
-    cross(ax3, f0y['slp']/tof, f1y['slp']/tof, 'y-plane')
+    sub(ax0, f0x['ini']/sd_x, f0x['slp']/tof, f1x['slp']/tof, 'horizontal plane')
+    cross(ax2, f0x['slp']/tof, f1x['slp']/tof, '')
+    sub(ax1, f0y['ini']/sd_y, f0y['slp']/tof, f1y['slp']/tof, 'vertical plane')
+    cross(ax3, f0y['slp']/tof, f1y['slp']/tof, '')
     plt.show()
     
     
